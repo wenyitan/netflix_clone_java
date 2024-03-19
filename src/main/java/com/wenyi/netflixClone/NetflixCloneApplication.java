39830df9f9cc43a52b56d94fddeb543ecd91aa4d
wenyitan <tanwenyi1511@gmail.com>
2024-03-19 19:55:50 +0800

    initial commit

---
+++ src/main/java/com/wenyi/netflixClone/NetflixCloneApplication.java
@@ -0,0 +1,13 @@
+package com.wenyi.netflixClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCloneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wenyi/netflixClone/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.wenyi.netflixClone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/login")
+    public String returnHelloWorld() {
+        return "login";
+    }
+}
